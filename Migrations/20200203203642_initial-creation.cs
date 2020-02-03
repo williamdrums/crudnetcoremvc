9ea5449b0106186dc3a3f33aaa218f9d8d7ddd19
unknown <williandrums@gmail.com>
2020-02-03 19:29:39 -0400

    crud asp.net core mvc e EF core

---
+++ Migrations/20200203203642_initial-creation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoa.Migrations
+{
+    public partial class initialcreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    IdPessoa = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(nullable: true),
+                    sobrenome = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: false),
+                    idade = table.Column<DateTime>(nullable: false),
+                    telefone = table.Column<string>(nullable: false),
+                    localizacao = table.Column<string>(nullable: false),
+                    cpf = table.Column<string>(nullable: true),
+                    hobby = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.IdPessoa);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int IdPessoa { get; set; }
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
+        [Display(Name = "Sobrenome")]
+        public string sobrenome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Display(Name= "Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime idade { get; set; }
+
+        [Phone]
+        [Required]
+        [Display(Name = "Telefone")]
+        public string telefone { get; set; }
+        [Required]
+        [Display(Name = "Localizacao")]
+        public string localizacao { get; set; }
+
+        [Display(Name = "Cpf")]
+        public string cpf { get; set; }
+
+        [Display(Name = "Hobby")]
+        public string hobby { get; set; }
+    }
+}
